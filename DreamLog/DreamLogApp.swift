7b7ac35379437bfb681b0d51dc005f85cab1be3d
Alexander Magana <alexander.jm28@gmail.com>
2021-05-11 22:15:01 -0500

    Initial Commit

---
+++ DreamLog/DreamLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamLogApp.swift
+//  DreamLog
+//
+//  Created by Alexander Magana on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DreamLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
